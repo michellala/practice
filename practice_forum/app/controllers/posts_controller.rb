cc5c0898a4dc08a4ccd42cb64b4ae2b3e753f013
Michelle Alarie <alarie.michelle@gmail.com>
2016-09-06 21:17:08 -0400

    Random post input/dummy data

--- practice_forum/app/controllers/posts_controller.rb
+++ practice_forum/app/controllers/posts_controller.rb
@@ -1,4 +1,34 @@
 class PostsController < ApplicationController
   def index
+    @posts = [
+      {
+        title: "First post",
+        author: "Author of Post",
+        text: <<--eos.gsub(/\s+/,"").strip
+          This is where the text goes for the post.
+          eos
+      },
+      {
+        title: "Second post",
+        author: "Author of Second Post",
+        text: <<--eos.gsub(/\s+/,"").strip
+          This is where the text goes for the post.
+          eos
+      },
+      {
+        title: "Third post",
+        author: "Author of Third Post",
+        text: <<--eos.gsub(/\s+/,"").strip
+          This is where the text goes for the post.
+          eos
+      },
+      {
+        title: "Fourth post",
+        author: "Author of FourthPost",
+        text: <<--eos.gsub(/\s+/,"").strip
+          This is where the text goes for the post.
+          eos
+      },
+    ]
   end
 end
