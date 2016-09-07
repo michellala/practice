87a21e22e69c7602fa649f91e2d11c6b06904ed2
Michelle Alarie <alarie.michelle@gmail.com>
2016-09-06 21:23:24 -0400

    Added posts to view erb file

--- practice_forum/app/controllers/posts_controller.rb
+++ practice_forum/app/controllers/posts_controller.rb
@@ -4,28 +4,28 @@ class PostsController < ApplicationController
       {
         title: "First post",
         author: "Author of Post",
-        text: <<--eos.gsub(/\s+/,"").strip
+        text: <<-eos.gsub(/\s+/," ").strip
           This is where the text goes for the post.
           eos
       },
       {
         title: "Second post",
         author: "Author of Second Post",
-        text: <<--eos.gsub(/\s+/,"").strip
+        text: <<-eos.gsub(/\s+/,"").strip
           This is where the text goes for the post.
           eos
       },
       {
         title: "Third post",
         author: "Author of Third Post",
-        text: <<--eos.gsub(/\s+/,"").strip
+        text: <<-eos.gsub(/\s+/,"").strip
           This is where the text goes for the post.
           eos
       },
       {
         title: "Fourth post",
         author: "Author of FourthPost",
-        text: <<--eos.gsub(/\s+/,"").strip
+        text: <<-eos.gsub(/\s+/,"").strip
           This is where the text goes for the post.
           eos
       },
